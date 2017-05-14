fb182bc936f917dbe5d1250b2489a26dc87ae8ac
Rafael <rmboloix@gmail.com>
2017-05-14 08:54:03 +0200

    eslint corrections

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Col } from 'react-bootstrap';
 import AtrapaloLogo from './AtrapaloLogo';
-import {Col} from 'react-bootstrap';
 
 const Header = () => {
-  let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-  //#de0000
+  // #de0000
   return (
     <div>
       <AtrapaloLogo />
-      <Col md={8} style={{margin:'0 auto'}}>
-        <h1 style={{textAlign: 'right', verticalAlign: 'middle', color:'white'}}
-            className="right">
+      <Col md={8} style={{ margin: '0 auto' }}>
+        <h1
+          style={{ textAlign: 'right', verticalAlign: 'middle', color: 'white' }}
+          className="right"
+        >
           DT - TECH LIBRARY
         </h1>
-        <span style={{float:'right', color:'white'}}>{new Date().toLocaleDateString('es-ES', options)}</span>
+        <span style={{ float: 'right', color: 'white' }}>{ new Date().toLocaleDateString('es-ES', options) }</span>
       </Col>
     </div>
   );
