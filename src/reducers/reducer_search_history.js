f269dbec7b7558d97f22a17e5f03b59ec4749646
Rafael <rmboloix@gmail.com>
2017-05-21 11:50:21 +0200

    limit search history

--- src/reducers/reducer_search_history.js
+++ src/reducers/reducer_search_history.js
@@ -5,7 +5,7 @@ export default function (state = [], action) {
   switch (action.type) {
     case SEARCH_REQUEST: {
       const { searchTerm, searchType } = action.payload;
-      return _.uniqWith([...state, { searchTerm, searchType }], _.isEqual);
+      return _.uniqWith([...state, { searchTerm, searchType }], _.isEqual).splice(-5);
     }
     default:
       return state;
