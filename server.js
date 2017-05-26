9b1227b6eb34c164a3adcdd3b6b0b765bb76748d
Rafael <rmboloix@gmail.com>
2017-05-26 18:46:35 +0200

    server compression

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.static(__dirname));
+app.use(compression());
 
 app.get('*', (req, res) => {
     let index = path.join(__dirname + '/index.html');
