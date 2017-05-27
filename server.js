c14f7f72ea94a0c3c20e00e91eb4758e5e254113
Rafael <rmboloix@gmail.com>
2017-05-27 08:38:55 +0200

    https redirect

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express');
 const compression = require('compression');
 const path = require('path');
 const port = process.env.PORT || 8080;
+const env = process.env.NODE_ENV || 'development';
 
 const app = express();
 
 app.use(express.static(__dirname));
 app.use(compression());
+
+app.get('*', function (req, res, next) {
+  if (
+    req.headers['x-forwarded-proto'] !== 'https' &&
+    env === 'production'
+  ) {
+    res.redirect('https://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.get('*', (req, res) => {
     let index = path.join(__dirname + '/index.html');
